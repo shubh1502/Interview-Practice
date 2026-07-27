8e7c82224d3d532b9bbe1cf28242fa2741e518f5
Shubham <98806385+shubh1502@users.noreply.github.com>
2026-07-27 20:19:33 +0000

    Add function to reverse a string while preserving special characters

--- Javascript/strings/string_manipulation.js
+++ Javascript/strings/string_manipulation.js
@@ -21,3 +21,32 @@ const desiredoutput = (string) =>{
 }
 
 console.log(desiredoutput(input))
+
+// -------------------------------------------------------------
+
+// Reverse a string without changing position of any special character.
+const input2 = "R@*IG#H!T";
+// Output- "T@*HG#I!R"
+
+const desiredoutput2 = (string) =>{
+    const arrstr = string.split('')
+    let left = 0
+    let right = arrstr.length - 1
+
+    while(left < right){
+        if(!/[a-zA-Z]/.test(arrstr[left])){
+            left++
+        }
+        else if(!/[a-zA-Z]/.test(arrstr[right])){
+            right--
+        }
+        else{
+            [arrstr[left], arrstr[right]] = [arrstr[right], arrstr[left]]
+            left++
+            right--
+        }
+    }
+    return arrstr.join('')
+}
+
+console.log(desiredoutput2(input2))
