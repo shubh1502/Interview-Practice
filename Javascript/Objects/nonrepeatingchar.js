63a5608011ebca8458edec65bea01d2dfc6d8f50
Shubham <98806385+shubh1502@users.noreply.github.com>
2026-08-13 10:53:23 +0000

    feat: add function to find the nth non-repeating character in a string

---
+++ Javascript/Objects/nonrepeatingchar.js
@@ -0,0 +1,25 @@
+let name = 'Shubhamm'
+
+const desiredOutput = (str, pos)=>{
+    const stack = [...str].reduce((acc,curr)=>{
+        acc[curr] = acc[curr] ? acc[curr]+1 : 1
+        return acc
+    },{})
+    let result = ''
+    console.log(stack);
+    let start = 1
+    for(let char of str){
+        if(stack[char] == 1){
+            if(start < pos ){
+                start++
+            }
+            else {
+                return result = char
+                start = str.length()
+            }
+        }
+    }
+    return result
+}
+
+console.log(desiredOutput(name,3));
