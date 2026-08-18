a7b1d6ef54448b3b51aba2a095d319910d875700
Shubham <98806385+shubh1502@users.noreply.github.com>
2026-08-18 21:09:11 +0000

    feat: add optimized twoSum function and retain brute force implementation

--- Javascript/Arrays/Two_Sum.js
+++ Javascript/Arrays/Two_Sum.js
@@ -4,6 +4,7 @@
 const nums = [2, 7, 11, 15];
 const target = 17;
 
+//Brute Force
 const twoSum = (nums, target) =>{
     if(!Array.isArray(nums)){
         throw new Error(`${nums} is not an array`)
@@ -20,4 +21,23 @@ const twoSum = (nums, target) =>{
     return finalarr
 }
 
-console.log(twoSum(nums,target));
+
+//Optimized solution
+const twoSum_optimized = (nums,target) =>{
+    const seen = new Map()
+
+
+    for (let i=0;i<nums.length;i++){
+        const current = nums[i]
+        const complement = target - current
+
+        if(seen.has(complement)){
+            return [seen.get(complement),i]
+        }
+
+        seen.set(current,i)
+    }
+    return []
+}
+
+console.log(twoSum_optimized(nums,target));
